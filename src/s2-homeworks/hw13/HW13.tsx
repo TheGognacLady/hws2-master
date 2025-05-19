56fc97fc3f2810518853643c777ee7031c412564
JaneN <TheCognacLady@gmail.com>
2025-05-19 15:01:30 +0300

    homework_13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -15,127 +15,127 @@ import errorUnknown from './images/error.svg'
 * 3 - сделать стили в соответствии с дизайном
 * */
 
-// const HW13 = () => {
-//     const [code, setCode] = useState('')
-//     const [text, setText] = useState('')
-//     const [info, setInfo] = useState('')
-//     const [image, setImage] = useState('')
-//     const [dis, setDis] = useState(false)
-//
-//     const send = (x?: boolean | null) => () => {
-//         setDis(true)
-//         const url =
-//             x === null
-//                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-//                 : 'https://samurai.it-incubator.io/api/3.0/homework/test'
-//
-//         setCode('')
-//         setImage(errorUnknown)
-//
-//         setInfo('...loading')
-//
-//         axios
-//             .post(url, {success: x})
-//             .then((res) => {
-//                 setText(res.data.errorText)
-//                 setCode(`Код ${res.status} !!!!`)
-//                 setImage(success200)
-//                 setInfo(res.data.info)
-//
-//
-//             })
-//             .catch((e) => {
-//
-//                 if (e.response.data){
-//                     setText(e.response.data.errorText)
-//                     setInfo(e.response.data.info)
-//
-//                     if (e.response.status ===400){
-//                         setCode(e.response.status)
-//                         setImage(error400)
-//                     } if (e.response.status ===500){
-//                         setCode(e.response.status)
-//                         setImage(error500)
-//                     }
-//                 } else {
-//
-//                     setText(e.message)
-//                     setInfo(e.name)
-//                 }
-//
-//             }).finally(()=>{
-//             setDis(false)
-//         })
-//
-//     }
-//     console.log(dis)
-//     return (
-//         <div id={'hw13'}>
-//             <div className={s2.hwTitle}>Homework #13</div>
-//
-//             <div className={s2.hw}>
-//                 <div className={s.buttonsContainer}>
-//                     <SuperButton
-//                         id={'hw13-send-true'}
-//                         onClick={send(true)}
-//                         xType={'secondary'}
-//                         disabled={dis}
-//
-//                     >
-//                         Send true
-//                     </SuperButton>
-//                     <SuperButton
-//                         id={'hw13-send-false'}
-//                         onClick={send(false)}
-//                         xType={'secondary'}
-//                         disabled={dis}
-//
-//
-//                     >
-//                         Send false
-//                     </SuperButton>
-//                     <SuperButton
-//                         id={'hw13-send-undefined'}
-//                         onClick={send(undefined)}
-//                         xType={'secondary'}
-//                         disabled={dis}
-//
-//
-//                     >
-//                         Send undefined
-//                     </SuperButton>
-//                     <SuperButton
-//                         id={'hw13-send-null'}
-//                         onClick={send(null)} // имитация запроса на не корректный адрес
-//                         xType={'secondary'}
-//                         disabled={dis}
-//
-//
-//                     >
-//                         Send null
-//                     </SuperButton>
-//                 </div>
-//
-//                 <div className={s.responseContainer}>
-//                     <div className={s.imageContainer}>
-//                         {image && <img src={image} className={s.image} alt="status"/>}
-//                     </div>
-//
-//                     <div className={s.textContainer}>
-//                         <div id={'hw13-code'} className={s.code}>
-//                             {code}
-//                         </div>
-//                         <div id={'hw13-text'} className={s.text}>
-//                             {text}
-//                         </div>
-//                         <div id={'hw13-info'} className={s.info}>
-//                             {info}
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-//
-// export default HW13
+const HW13 = () => {
+    const [code, setCode] = useState('')
+    const [text, setText] = useState('')
+    const [info, setInfo] = useState('')
+    const [image, setImage] = useState('')
+    const [dis, setDis] = useState(false)
+
+    const send = (x?: boolean | null) => () => {
+        setDis(true)
+        const url =
+            x === null
+                ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
+                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
+
+        setCode('')
+        setImage(errorUnknown)
+
+        setInfo('...loading')
+
+        axios
+            .post(url, {success: x})
+            .then((res) => {
+                setText(res.data.errorText)
+                setCode(`Код ${res.status} !!!!`)
+                setImage(success200)
+                setInfo(res.data.info)
+
+
+            })
+            .catch((e) => {
+
+                if (e.response.data){
+                    setText(e.response.data.errorText)
+                    setInfo(e.response.data.info)
+
+                    if (e.response.status ===400){
+                        setCode(e.response.status)
+                        setImage(error400)
+                    } if (e.response.status ===500){
+                        setCode(e.response.status)
+                        setImage(error500)
+                    }
+                } else {
+
+                    setText(e.message)
+                    setInfo(e.name)
+                }
+
+            }).finally(()=>{
+            setDis(false)
+        })
+
+    }
+    console.log(dis)
+    return (
+        <div id={'hw13'}>
+            <div className={s2.hwTitle}>Homework #13</div>
+
+            <div className={s2.hw}>
+                <div className={s.buttonsContainer}>
+                    <SuperButton
+                        id={'hw13-send-true'}
+                        onClick={send(true)}
+                        xType={'secondary'}
+                        disabled={dis}
+
+                    >
+                        Send true
+                    </SuperButton>
+                    <SuperButton
+                        id={'hw13-send-false'}
+                        onClick={send(false)}
+                        xType={'secondary'}
+                        disabled={dis}
+
+
+                    >
+                        Send false
+                    </SuperButton>
+                    <SuperButton
+                        id={'hw13-send-undefined'}
+                        onClick={send(undefined)}
+                        xType={'secondary'}
+                        disabled={dis}
+
+
+                    >
+                        Send undefined
+                    </SuperButton>
+                    <SuperButton
+                        id={'hw13-send-null'}
+                        onClick={send(null)} // имитация запроса на не корректный адрес
+                        xType={'secondary'}
+                        disabled={dis}
+
+
+                    >
+                        Send null
+                    </SuperButton>
+                </div>
+
+                <div className={s.responseContainer}>
+                    <div className={s.imageContainer}>
+                        {image && <img src={image} className={s.image} alt="status"/>}
+                    </div>
+
+                    <div className={s.textContainer}>
+                        <div id={'hw13-code'} className={s.code}>
+                            {code}
+                        </div>
+                        <div id={'hw13-text'} className={s.text}>
+                            {text}
+                        </div>
+                        <div id={'hw13-info'} className={s.info}>
+                            {info}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HW13
