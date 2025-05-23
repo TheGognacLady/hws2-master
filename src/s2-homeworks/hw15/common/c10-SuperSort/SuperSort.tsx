a94489c4928544d565d5cda2b6ad4b94253e5f6b
JaneN <TheCognacLady@gmail.com>
2025-05-23 22:33:39 +0300

    homework_14

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -4,71 +4,71 @@ import icon2 from "../../../../assets/images.png"
 import icon3 from "../../../../assets/CodeWarsLogo.jpg"
 import s from "../../HW15.module.css"
 // добавить в проект иконки и импортировать
-// const downIcon = icon1
-// const upIcon = icon2
-// const noneIcon = icon3
-//
-// export type SuperSortPropsType = {
-//     id?: string
-//     sort: string
-//     value: string
-//     onChange: (newSort: string) => void
-// }
-//
-// export const pureChange = (sort: string, down: string, up: string) => {
-//     // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-//     console.log(sort)
-//     let newSort = ""
-// /*if (sort===""){
-//     newSort = up
-// } if (sort === up){
-//         newSort = down
-//     } if (sort === down){
-//         newSort = ""
-//     }*/
-// /*if(sort === "") {
-//     newSort = down
-// } else if(sort === down) {
-//     newSort = up
-// } else {
-//     newSort = ""
-// }*/
-//
-//     return sort=== down? up : sort === up ? "" : down
-//
-// }
-//
-// const SuperSort: React.FC<SuperSortPropsType> = (
-//     {
-//         sort, value, onChange, id = 'hw15',
-//     }
-// ) => {
-//     const up = '0' + value
-//     const down = '1' + value
-//
-//     const onChangeCallback = () => {
-//         onChange(pureChange(sort, down, up))
-//     }
-//
-//     const icon = sort === down
-//         ? downIcon
-//         : sort === up
-//             ? upIcon
-//             : noneIcon
-//
-//     return (
-//         <span
-//             id={id + '-sort-' + value}
-//             onClick={onChangeCallback}
-//         >
-//
-//             <img className={s.img}
-//                 id={id + '-icon-' + sort}
-//                 src={icon}
-//             />
-//
-//         </span>
-//     )
-// }
-//
-// export default SuperSort
+const downIcon = icon1
+const upIcon = icon2
+const noneIcon = icon3
+
+export type SuperSortPropsType = {
+    id?: string
+    sort: string
+    value: string
+    onChange: (newSort: string) => void
+}
+
+export const pureChange = (sort: string, down: string, up: string) => {
+    // пишет студент,sort: (click) => down (click) => up (click) => '' (click) => down ...
+    console.log(sort)
+    let newSort = ""
+if (sort===""){
+    newSort = up
+} if (sort === up){
+        newSort = down
+    } if (sort === down){
+        newSort = ""
+    }
+if(sort === "") {
+    newSort = down
+} else if(sort === down) {
+    newSort = up
+} else {
+    newSort = ""
+}
+
+    return sort=== down? up : sort === up ? "" : down
+
+}
+
+const SuperSort: React.FC<SuperSortPropsType> = (
+    {
+        sort, value, onChange, id = 'hw15',
+    }
+) => {
+    const up = '0' + value
+    const down = '1' + value
+
+    const onChangeCallback = () => {
+        onChange(pureChange(sort, down, up))
+    }
+
+    const icon = sort === down
+        ? downIcon
+        : sort === up
+            ? upIcon
+            : noneIcon
+
+    return (
+        <span
+            id={id + '-sort-' + value}
+            onClick={onChangeCallback}
+        >
+
+            <img className={s.img}
+                id={id + '-icon-' + sort}
+                src={icon}
+            />
+
+        </span>
+    )
+}
+
+export default SuperSort
